5a6ed1c35ab5559dd0e0e1c9e9cc5cf163e85a27
Kal Adam Kalo <wx1@outlook.dk>
2024-11-06 16:45:06 +0100

    Update 9_02327DatabaseScript2_2024.sql

---
+++ 9_02327DatabaseScript2_2024.sql
@@ -0,0 +1,4 @@
+-- It must contain
+-- (1) the delete/update statements used to change the tables (as in section 5), and
+-- (2) the queries made (as in section 6), and
+--(3) the statements used to create and apply functions, procedures, triggers, and events (as in section 7)
