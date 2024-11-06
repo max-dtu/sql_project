17c4109a3dbcb444ffb602bb946da9175d768cfd
Kal Adam Kalo <wx1@outlook.dk>
2024-11-06 16:44:26 +0100

    Update 9_02327DatabaseScript1_2024.sql

---
+++ 9_02327DatabaseScript1_2024.sql
@@ -0,0 +1,4 @@
+
+-- It must contain
+-- (1) the statements used to create the database, its tables and views (as used in section 3 of the report)
+-- (2) the statements used to populate the tables (as used in section 4)
