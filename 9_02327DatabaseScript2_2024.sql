94de3f1ef2c909181ff2633768b978a0ddee3f37
Kal Adam Kalo <wx1@outlook.dk>
2024-11-08 23:52:20 +0100

    Update 9_02327DatabaseScript2_2024.sql

--- 9_02327DatabaseScript2_2024.sql
+++ 9_02327DatabaseScript2_2024.sql
@@ -1,7 +1,7 @@
 -- It must contain:
   -- (1) the delete/update statements used to change the tables (as in section 5)
+
   -- (2) the queries made (as in section 6)
-  
     -- Answer the following questions by writing appropriate SQL table queries:
       -- Show the ID of the passengers who took a ride from the first stop of the line taken.
       -- Show the name of the bus stop served my most lines.
