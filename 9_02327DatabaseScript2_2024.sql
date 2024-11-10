e81f68493e7cc0ce006f9a82c9bf293467edd1e6
truthpapers <116232689+truthpapers@users.noreply.github.com>
2024-11-10 13:29:58 +0100

    add the output of each command as required in the assignment.

--- 9_02327DatabaseScript2_2024.sql
+++ 9_02327DatabaseScript2_2024.sql
@@ -3,11 +3,38 @@
   INSERT INTO Stops (stop_id, stop_name, GPS)
 VALUES (107, 'DTU Lyngby Campus', '40.730610,-73.935242');
 
+select * from Stops;
+-- reslut after adding the above row:
+	-- stop_id stop_name 	GPS
+	-- '101', 'Central Station', '40.748817,-73.985428'
+	-- '102', 'City Hall', '40.712776,-74.005974'
+	-- '103', 'Museum of Art', '40.779437,-73.963244'
+	-- '104', 'University Campus', '40.730610,-73.935242'
+	-- '105', 'Park Avenue', '40.754932,-73.984016'
+	-- '106', 'DStop', '40.754932,-73.984019'
+	-- '107', 'DTU Lyngby Campus', '40.730610,-73.935242'
+
 UPDATE Stops
 SET GPS = '40.712776,-74.0060'  -- New GPS coordinates for City Hall
 WHERE stop_id = 102;
 
+select * from Stops;
+-- reslut after executing the above UPDATE command:
+	-- stop_id stop_name 	GPS
+	-- '102', 'City Hall', '40.712776,-74.0060'
+
+
 DELETE FROM Stops WHERE stop_id = 107;
+
+select * from Stops;
+-- reslut after executing the above DELETE command:
+	-- stop_id stop_name 	GPS
+	-- '101', 'Central Station', '40.748817,-73.985428'
+	-- '102', 'City Hall', '40.712776,-74.005974'
+	-- '103', 'Museum of Art', '40.779437,-73.963244'
+	-- '104', 'University Campus', '40.730610,-73.935242'
+	-- '105', 'Park Avenue', '40.754932,-73.984016'
+	-- '106', 'DStop', '40.754932,-73.984019'
 
   -- (2) the queries made (as in section 6)
     -- Answer the following questions by writing appropriate SQL table queries:
@@ -15,22 +42,48 @@ DELETE FROM Stops WHERE stop_id = 107;
       select * from Line_Stops;
       select * from Rides, Line_Stops where Line_Stops.stop_order = 1;
       select distinct card_id from Rides, Line_Stops where Line_Stops.stop_order = 1;
+		-- output:
+        -- card_id
+        -- 1001
+        -- 1002
+        -- 1003
+        -- 1004
+        -- 1005
       -- Show the name of the bus stop served my most lines.
        select * from Stops;
       select * from Line_Stops;
       select stop_id, count(Line_Stops.stop_id) from Line_Stops group by Line_Stops.stop_id LIMIT 1;
+      -- output:
+      -- stop_id
+      -- 101
       
       -- For each line, show the ID of the passenger who took the ride that lasted longer.
       select * from Rides;
-      select card_id, MAX(duration) from Rides group by line_id;
+      select card_id, MAX(duration), line_id from Rides group by line_id;
+      -- output:
+      -- card_id MAX(duration) line_id
+      -- 1001 40 1
+      -- 1004 30 2
+      -- 1003 30 3
 
       -- Show the ID of the passengers who never took a bus line more than once per day.
       select *, COUNT(card_id) from Rides group by start_date;
       SELECT distinct card_id FROM Rides GROUP BY card_id, line_id, start_date HAVING COUNT(ride_id) = 1;
+      -- output:
+      -- card_id
+      -- 1001
+      -- 1002
+      -- 1003
+      -- 1005
+     
+	
 
       -- Show the name of the bus stops that are never used, that is, they are neither the start nor the end stop for any ride.
     select stop_id from Stops where stop_id not in (SELECT distinct on_stop_id FROM Rides UNION SELECT DISTINCT off_stop_id FROM Rides);
-    
+    -- output:
+    -- stop_id
+    -- 105
+    -- 106
     
   -- (3) the statements used to create and apply functions, procedures, triggers, and events (as in section 7)
       -- Implement and explain what they do:
