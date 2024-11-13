ba1ecb477e9c34691d2b4daa643b9237e411c46b
arthur-d42 <arthurdiness@gmail.com>
2024-11-13 12:52:15 +0100

    Changed data to måneby

--- 9_02327DatabaseScript1_2024.sql
+++ 9_02327DatabaseScript1_2024.sql
@@ -79,48 +79,48 @@ CREATE TABLE PhoneNumbers (
 
  
  INSERT INTO Line (line_id, line_name, final_destination) VALUES
-(1, 'Downtown Express', 103),
-(2, 'City Circle', 101),
-(3, 'North Route', 104),
-(4, 'South Shuttle', 101),
-(5, 'Land Road', 105);
+(1, 'Kajkage express', 103),
+(2, 'Ydre måneby rute', 101),
+(3, 'Troldmandskvarteret Syd', 104),
+(4, 'Troldmandskvarteret Nord', 101),
+(5, 'Grottevej', 105);
 
 INSERT INTO Stops (stop_id, stop_name, GPS) VALUES
-(101, 'Central Station', '40.748817,-73.985428'),
-(102, 'City Hall', '40.712776,-74.005974'),
-(103, 'Museum of Art', '40.779437,-73.963244'),
-(104, 'University Campus', '40.730610,-73.935242'),
-(105, 'Park Avenue', '40.754932,-73.984016'),
-(106, 'DStop', '40.754932,-73.984019');
+(101, 'Bermuda Triangle', '25.0000,-71.0000'),
+(102, 'Krateret', '24.9640,-71.0205'),
+(103, 'Grotten', '25.0306,-70.9632'),
+(104, 'Lunar Campus', '24.9818,-70.9352'),
+(105, 'Rådhuset', '25.0061,-70.9840'),
+(106, 'The Best Stop', '25.0061,-70.9840');
 
 INSERT INTO Addresses (address_id, country, city, zip, street_name, civic_number) VALUES
-(111, 'USA', 'New York', 10001, '5th Ave', '10A'),
-(112, 'USA', 'New York', 10002, 'Broadway', '20B'),
-(113, 'USA', 'New York', 10003, 'Wall St', '30C');
+(111, 'Bermuda', 'Måneby', 10001, 'Grottevej', '10A'),
+(112, 'Bermuda', 'Måneby', 10002, 'Troldestræde', '20B'),
+(113, 'Bermuda', 'Måneby', 10003, 'Krater Allé', '30C');
 
 INSERT INTO Passengers (card_id, email, first_name, last_name, address_id) VALUES
-(1001, 'john.doe@ex.com', 'John', 'Doe', 111),
-(1002, 'jane.smith@ex.com', 'Jane', 'Smith', 111),
-(1003, 'sam.wilson@ex.com', 'Sam', 'Wilson', 112),
-(1004, 'emily.jones@ex.com', 'Emily', 'Jones', 113),
-(1005, 'alice.jones@ex.com', 'Alice', 'Jones', 113);
+(1001, 'martin@bitfrost.com', 'Martin', 'Bitfrost', 111),
+(1002, 'martinogmette@posteo.dk', 'Martin', 'Grønbech', 111),
+(1003, 'gottago@fastmail.com', 'Sonic', 'Thehedgehod', 112),
+(1004, 'xxx_Shadow_xxx@ex.com', 'Shadow', 'Shadowsen', 113),
+(1005, 'gothboy96@gmail.com', 'Gothboy', 'Ninentysix', 113);
 
 INSERT INTO Rides (card_id, ride_id, start_date, start_time, duration, on_stop_id, off_stop_id, line_id) VALUES
-(1001, 50001, '2023-10-01', '08:00:00', 30, 101, 102, 1),
+(1001, 50001, '2024-10-01', '08:00:00', 30, 101, 102, 1),
  
-(1002, 50001, '2023-10-01', '08:00:00', 30, 101, 102, 1), 
+(1002, 50001, '2024-10-01', '08:00:00', 30, 101, 102, 1), 
 
-(1003, 50002, '2023-10-01', '08:00:00', 40, 101, 103, 1), -- the longest ride (40 minutes)
+(1003, 50002, '2024-10-01', '08:00:00', 40, 101, 103, 1), -- the longest ride (40 minutes)
 
-(1003, 50003, '2023-10-01', '09:00:00', 30, 101, 104, 3),
+(1003, 50003, '2024-10-01', '09:00:00', 30, 101, 104, 3),
 
-(1004, 50004, '2023-10-02', '09:30:00', 20, 102, 103, 2),
-(1004, 50005, '2023-10-02', '11:15:00', 20, 103, 102, 2),
-(1004, 50006, '2023-10-02', '11:15:00', 30, 103, 101, 2), -- the longst ride (30 minutes)
+(1004, 50004, '2024-10-02', '09:30:00', 20, 102, 103, 2),
+(1004, 50005, '2024-10-02', '11:15:00', 20, 103, 102, 2),
+(1004, 50006, '2024-10-02', '11:15:00', 30, 103, 101, 2), -- the longst ride (30 minutes)
 
-(1005, 50005, '2023-10-02', '11:15:00', 20, 103, 102, 2),
+(1005, 50005, '2024-10-02', '11:15:00', 20, 103, 102, 2),
 
-(1005, 50007, '2023-10-03', '11:15:00', 20, 103, 102, 2);
+(1005, 50007, '2024-10-03', '11:15:00', 20, 103, 102, 2);
 
 INSERT INTO Stops_Line (line_id, stop_id, stop_order) VALUES
 (1, 101, 1),
@@ -152,10 +152,10 @@ INSERT INTO Stops_Passengers (stop_id, card_id) VALUES
 
 
 INSERT INTO PhoneNumbers (number, card_id) VALUES
-('1234567890', 1001), -- John Doe's phone number
-('0987654321', 1001), -- John Doe has multiple numbers
-('1122334455', 1002), -- Jane Smith's phone number
-('2233445566', 1003); -- Sam Wilson's phone number
+('1234567890', 1001),
+('0987654321', 1001),
+('1122334455', 1002),
+('2233445566', 1003);
 
  ALTER TABLE Line
   ADD CONSTRAINT fk_final_destination FOREIGN KEY (final_destination) REFERENCES Stops(stop_id);
