e50b87fcbbde3653392553871ed55a4a674e46f9
Kal Adam Kalo <wx1@outlook.dk>
2024-11-16 21:12:53 +0100

    Update data queries after adding new tables in script2

--- 9_02327DatabaseScript2_2024.sql
+++ 9_02327DatabaseScript2_2024.sql
@@ -1,276 +1,191 @@
 -- It must contain:
-  -- (1) the delete/update statements used to change the tables (as in section 5)
-  INSERT INTO Stops (stop_id, stop_name, GPS)
-VALUES (107, 'DTU Lyngby Campus', '55.7849149,12.5219711');
+-- (1) the statements used to create the database, its tables and views (as used in section 3 of the report)
+-- (2) the statements used to populate the tables (as used in section 4)
+
+DROP DATABASE IF EXISTS MoviBus;
+CREATE DATABASE MoviBus;
+USE MoviBus;
+
+CREATE TABLE Line (
+    line_id INT PRIMARY KEY,
+    line_name VARCHAR(255),
+    final_destination INT NOT NULL
+	-- FOREIGN KEY (final_destination) REFERENCES Stops(stop_id)
+);
+
+CREATE TABLE Stops (
+    stop_id INT PRIMARY KEY,
+    stop_name VARCHAR(255),
+    GPS VARCHAR(255)
+);
+
+
+
+CREATE TABLE Buses (
+    plate_number VARCHAR(20) PRIMARY KEY, 
+    model VARCHAR(50) NOT NULL,          
+    capacity INT NOT NULL,               
+    manufacture_year YEAR NOT NULL       
+);
+CREATE TABLE Addresses (
+    address_id INT PRIMARY KEY,
+    country VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    zip INT NOT NULL,
+    street_name VARCHAR(255) NOT NULL,
+    civic_number VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Passengers (
+    card_id INT PRIMARY KEY,
+    email VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES Addresses(address_id)
+);
+
+CREATE TABLE Rides_Passengers (
+    start_date DATE NOT NULL,           
+    start_time TIME NOT NULL,            
+    bus_id VARCHAR(20) NOT NULL,         
+    card_id INT NOT NULL,                
+    PRIMARY KEY (start_date, start_time, bus_id, card_id), 
+    FOREIGN KEY (bus_id) REFERENCES Buses(plate_number),  
+	FOREIGN KEY (card_id) REFERENCES Passengers(card_id) 
+);
+
+
+
+CREATE TABLE Stops_Line (
+    line_id INT,
+    stop_id INT,
+    stop_order INT NOT NULL,
+    PRIMARY KEY (line_id, stop_id),
+    FOREIGN KEY (line_id) REFERENCES Line(line_id),
+    FOREIGN KEY (stop_id) REFERENCES Stops(stop_id)
+);
+
+
+
+CREATE TABLE Rides (
+    start_date DATE NOT NULL,          
+    start_time TIME NOT NULL,         
+    bus_id VARCHAR(20) NOT NULL,       
+    duration INT NOT NULL,             
+    on_stop_id INT NOT NULL,         
+    off_stop_id INT NOT NULL,        
+    line_id INT NOT NULL,             
+    PRIMARY KEY (start_date, start_time, bus_id), -- Composite primary key
+    FOREIGN KEY (bus_id) REFERENCES Buses(plate_number), 
+    FOREIGN KEY (on_stop_id) REFERENCES Stops(stop_id),  
+    FOREIGN KEY (off_stop_id) REFERENCES Stops(stop_id), 
+    FOREIGN KEY (line_id) REFERENCES Line(line_id)       
+);
+
+
+
+CREATE TABLE Stops_Passengers (
+    stop_id INT,
+    card_id INT,
+    PRIMARY KEY (stop_id, card_id),
+    FOREIGN KEY (stop_id) REFERENCES Stops(stop_id),
+    FOREIGN KEY (card_id) REFERENCES Passengers(card_id)
+);
+
+CREATE TABLE PhoneNumbers (
+    number VARCHAR(20) PRIMARY KEY,
+    card_id INT NOT NULL,
+    FOREIGN KEY (card_id) REFERENCES Passengers(card_id)
+);
+
+ INSERT INTO Line (line_id, line_name, final_destination) VALUES
+    (1, 'Kajkage express', 103),
+    (2, 'Ydre måneby rute', 101),
+    (3, 'Troldmandskvarteret Syd', 104),
+    (4, 'Troldmandskvarteret Nord', 101),
+    (5, 'GummiKarsten Circus', 105);
+
 
 INSERT INTO Stops (stop_id, stop_name, GPS) VALUES
-(108, 'Rest stop 1', '35.2616383,-47.23901445'),
-(109, 'Rest stop 2', '45.5232766,-23.4780289'),
-(110, 'Klampenborgvej', '55.774613630806115, 12.524482144716382');
-
-select * from Stops;
--- reslut after adding the above row:
-	-- stop_id stop_name 	GPS
-	-- '101', 'Central Station', '40.748817,-73.985428'
-	-- '102', 'City Hall', '40.712776,-74.005974'
-	-- '103', 'Museum of Art', '40.779437,-73.963244'
-	-- '104', 'University Campus', '40.730610,-73.935242'
-	-- '105', 'Park Avenue', '40.754932,-73.984016'
-	-- '106', 'DStop', '40.754932,-73.984019'
-	-- '107', 'DTU Lyngby Campus', '40.730610,-73.935242'
-
-UPDATE Stops
-SET GPS = '55.7849149,12.5219711'  -- New GPS coordinates for City Hall
-WHERE stop_id = 102;
-
-select * from Stops;
--- reslut after executing the above UPDATE command:
-	-- stop_id stop_name 	GPS
-	-- '102', 'City Hall', '40.712776,-74.0060'
+    (101, 'Bermuda Triangle', '25.0000,-71.0000'),
+    (102, 'Krateret', '24.9640,-71.0205'),
+    (103, 'Grotten', '25.0306,-70.9632'),
+    (104, 'Lunar Campus', '24.9818,-70.9352'),
+    (105, 'Rådhuset', '25.0061,-70.9840'),
+    (106, 'The Best Stop', '25.0061,-70.9840'),
+    (107, 'stop1', '25.0061,-70.9850'),
+    (108, 'stop2', '25.0061,-70.9870'),
+	(109, 'stop3', '25.0061,-70.8588'),
+	(110, 'stop4', '25.0061,-70.9444');
+    
 
 
-DELETE FROM Stops WHERE stop_id = 107;
+INSERT INTO Buses (plate_number, model, capacity, manufacture_year) VALUES
+    ('AB1234', 'Volvo 7900', 50, 2018),
+    ('CD5678', 'Mercedes Citaro', 40, 2019),
+    ('EF9012', 'MAN Lion’s City', 45, 2020),
+    ('GH3456', 'Scania Citywide', 55, 2017),
+    ('IJ7890', 'BYD K9', 42, 2021);
 
-select * from Stops;
--- reslut after executing the above DELETE command:
-	-- stop_id stop_name 	GPS
-	-- '101', 'Central Station', '40.748817,-73.985428'
-	-- '102', 'City Hall', '40.712776,-74.005974'
-	-- '103', 'Museum of Art', '40.779437,-73.963244'
-	-- '104', 'University Campus', '40.730610,-73.935242'
-	-- '105', 'Park Avenue', '40.754932,-73.984016'
-	-- '106', 'DStop', '40.754932,-73.984019'
 
-  -- (2) the queries made (as in section 6)
-    -- Answer the following questions by writing appropriate SQL table queries:
-      -- Show the ID of the passengers who took a ride from the first stop of the line taken.
-      select * from Stops_Line;
-      select * from Rides, Stops_Line where Stops_Line.stop_order = 1;
-      select card_id from Rides, Stops_Line where Rides.on_stop_id = Stops_Line.stop_id and Stops_Line.stop_order = 1; -- This will do a cartesian join and then, it will only keep the rows with Rides.on_stop_id = Stops_Line.stop_id and on_stop_id order = 1, A passenger could take 2 lines, both from the start of the line.
-		-- output:
-        -- card_id
-        -- 1001
-        -- 1002
-        -- 1003
-        -- 1004
-        -- 1005
-      -- Show the name of the bus stop served my most lines.
-       select * from Stops;
-      select * from Stops_Line;
-      select stop_id, count(Stops_Line.stop_id) as stop_count from Stops_Line group by Stops_Line.stop_id ORDER BY stop_count DESC; -- Here we just count the stop_ids and order them and the top will be the name of the bus stop served by most lines.
-      -- output:
-      -- stop_id
-      -- 101
-      
-      -- For each line, show the ID of the passenger who took the ride that lasted longer.
-      select * from Rides;
-      select card_id, MAX(duration), line_id from Rides group by line_id; -- we group by line_id and then find the max duration for each group.
-      -- output:
-      -- card_id MAX(duration) line_id
-      -- 1001 40 1
-      -- 1004 30 2
-      -- 1003 30 3
+INSERT INTO Addresses (address_id, country, city, zip, street_name, civic_number) VALUES
+    (111, 'Bermuda', 'Måneby', 10001, 'Grottevej', '10A'),
+    (112, 'Bermuda', 'Måneby', 10002, 'Troldestræde', '20B'),
+    (113, 'Bermuda', 'Måneby', 10003, 'Krater Allé', '30C');
 
-      -- Show the ID of the passengers who never took a bus line more than once per day.
-      select *, COUNT(card_id) from Rides group by start_date;
-    SELECT distinct card_id FROM Rides GROUP BY card_id, line_id, start_date HAVING COUNT(ride_id) = 1; -- we sort out so that each group has distinct card_id, start_id, start_date, we get the following groups 
-      
--- '1001', '50001', '2024-10-01', '08:00:00', '30', '101', '102', '1'
 
--- '1002' , '50001', '2024-10-01', '08:00:00', '30', '101', '102', '1'
-
--- '1003' , '50002', '2024-10-01', '08:00:00', '40', '101', '103', '1'
-
--- '1003' , '50003', '2024-10-01', '09:00:00', '30', '101', '104', '3'
-
--- '1004' , '50004', '2024-10-02', '09:30:00', '20', '102', '103', '2'
--- '1004' , '50005', '2024-10-02', '11:15:00', '20', '103', '102', '2'
--- '1004' , '50006', '2024-10-02', '11:15:00', '30', '103', '101', '2'
-
--- '1005' , '50005', '2024-10-02', '11:15:00', '20', '103', '102', '2'
-
--- '1005' , '50007', '2024-10-03', '11:15:00', '20', '103', '102', '2'
-
--- then we start counting , we can see all the passengers having one count except passenger 1004
-
-      -- output:
-      -- card_id
-      -- 1001
-      -- 1002
-      -- 1003
-      -- 1005
-     
+INSERT INTO Passengers (card_id, email, first_name, last_name, address_id) VALUES
+    (1001, 'martin@bitfrost.com', 'Martin', 'Bitfrost', 111),
+    (1002, 'martinogmette@posteo.dk', 'Martin', 'Grønbech', 111),
+    (1003, 'gottago@fastmail.com', 'Sonic', 'Thehedgehog', 112),
+    (1004, 'xxx_Shadow_xxx@xxx.com', 'Shadow', 'Shadowsen', 113),
+    (1005, 'gothboy96@gmail.com', 'Gothboy', 'Ninentysix', 113);
 	
 
-      -- Show the name of the bus stops that are never used, that is, they are neither the start nor the end stop for any ride.
-       select stop_id from Stops where stop_id not in (SELECT distinct on_stop_id FROM Rides UNION SELECT DISTINCT off_stop_id FROM Rides); -- The subquery gets us a list of on_stops_id + off_stop_id, if a stop is not in tha list, then it's a stop that is not being used.
-    -- output:
-    -- stop_id
-    -- 105
-    -- 106
+
+INSERT INTO Rides (start_date, start_time, bus_id, duration, on_stop_id, off_stop_id, line_id) VALUES
+    ('2023-10-01', '08:00:00', 'AB1234', 30, 101, 104, 1), 
+    ('2023-10-01', '08:30:00', 'CD5678', 30, 101, 104, 1), 
     
-  -- (3) the statements used to create and apply functions, procedures, triggers, and events (as in section 7)
-      -- Implement and explain what they do:
-      --  A function that, given two stops, returns how many lines serve both stops.
-    DROP FUNCTION IF EXISTS get_lines;
-      -- drops the function if it already exists
-	DELIMITER //
-    -- changes delimeter so you can write a function.
-    CREATE FUNCTION get_lines(first_stop INT, second_stop INT) RETURNS INT
-    -- declares that get_lines() takes two ints and returns one int
-    BEGIN
-		DECLARE line_counter INT;
-        -- declares the line counter
-		SELECT COUNT(*) INTO line_counter
-		FROM ( SELECT line_id
-		FROM Stops_Line 
-		WHERE stop_id in (first_stop,second_stop)
-		GROUP BY line_id
-		HAVING COUNT(DISTINCT stop_id)=2)
-		AS lines_with_both;
-        RETURN line_counter;
-    END; //
-    DELIMITER ;
-	SELECT get_lines(104,101);
-    -- output:
-    -- get_lines(104,101)
-    -- 2
-    SELECT get_lines(102,103);
-    -- output:
-    -- get_lines(102,103)
-    -- 3
-    SELECT get_lines(101,105);
-    -- output:
-    -- get_lines(101,105)
-    -- 0
+    ('2023-10-02', '09:00:00', 'EF9012', 20, 103, 106, 3), 
+    ('2023-10-02', '09:30:00', 'GH3456', 15, 101, 106, 4);
+
+
+INSERT INTO Rides_Passengers (start_date, start_time, bus_id, card_id) VALUES
+
+	('2023-10-01', '08:00:00', 'AB1234', 1001), 
+    ('2023-10-01', '09:00:00', 'EF9012', 1001), 
     
-
-      --  A procedure that, given a line and a stop, adds the stop to that line (after the last stop) if not already served by that line
-DROP PROCEDURE IF EXISTS add_stop;
-DELIMITER //
-CREATE PROCEDURE add_stop(new_line_id INT, new_stop_id INT)
-    BEGIN
-		DECLARE max_stop_order INT;
-        SELECT MAX(stop_order)
-        INTO max_stop_order
-        FROM Stops_Line
-        WHERE line_id=new_line_id;
-        
-		IF NOT EXISTS (
-			SELECT 1
-			FROM Stops_Line
-			WHERE line_id=new_line_id AND stop_id=new_stop_id
-            )THEN
-            INSERT INTO Stops_Line(line_id,stop_id,stop_order)
-			VALUES (new_line_id, new_stop_id, max_stop_order+1);
-	END IF;
-    END//
-	DELIMITER ;
-    CALL add_stop(1,103);
-	SELECT * FROM Stops_line;
-    -- output:
-    -- Original Stop_line
-	CALL add_stop(1,104);
-	SELECT * FROM Stops_line;
-    -- output:
-    -- Stop_lines with
-    -- line_id 1
-    -- stop_id 104
-    -- stop_order 4
+    ('2023-10-01', '08:00:00', 'AB1234', 1002), 
     
-      --  A trigger that prevents inserting a ride starting and ending at the same stop, or at a stop not served by that line.
-    DELIMITER //
-	CREATE TRIGGER ride_trigger
-      BEFORE INSERT ON Rides
-      FOR EACH ROW
-      BEGIN
-      -- the part of the trigger that prevents getting off on the stop you got on
-      IF NEW.on_stop_id=NEW.off_stop_id
-      THEN SIGNAL SQLSTATE 'HY000'
-			SET MYSQL_ERRNO = 1525,
-			MESSAGE_TEXT = 'The end stop is equal to start stop';
-      END IF;
-      -- The trigger that prevents getting on a stop that is not on the line
-      IF NEW.on_stop_id NOT IN 
-      (SELECT stop_id FROM Stops_Line
-      WHERE line_id=NEW.line_id)
-      THEN SIGNAL SQLSTATE 'HY000'
-			SET MYSQL_ERRNO = 1525,
-			MESSAGE_TEXT = 'The on stop is not on the line';
-      END IF;
-      -- The trigger preventing getting off on a stop that is not on the line
-      IF NEW.off_stop_id NOT IN 
-      (SELECT stop_id FROM Stops_Line
-      WHERE line_id=NEW.line_id)
-      THEN SIGNAL SQLSTATE 'HY000'
-			SET MYSQL_ERRNO = 1525,
-			MESSAGE_TEXT = 'The end stop is not on the line';
-      END IF;
-      END//
-	DELIMITER ;
-    INSERT INTO Rides (card_id, ride_id,start_date,start_time,duration,on_stop_id,off_stop_id,line_id)
-    VALUES(1001,5008,'2024-11-13','10:00:00',20,101,101,2);
-    -- output
-    -- Error Code: 1525. The end stop is equal to start stop
-    INSERT INTO Rides (card_id, ride_id,start_date,start_time,duration,on_stop_id,off_stop_id,line_id)
-    VALUES(1001,5008,'2024-11-13','10:00:00',20,104,101,2);
-    -- output
-    -- Error Code: 1525. The on stop is not on the line
-	INSERT INTO Rides (card_id, ride_id,start_date,start_time,duration,on_stop_id,off_stop_id,line_id)
-    VALUES(1001,5008,'2024-11-13','10:00:00',20,101,104,2);
-    -- Error Code: 1525. The end stop is not on the line
-    INSERT INTO Rides (card_id, ride_id,start_date,start_time,duration,on_stop_id,off_stop_id,line_id)
-    VALUES(1001,5008,'2024-11-13','10:00:00',20,101,102,2);
-    SELECT * FROM Rides;
-    -- output
-    -- Same Rides as before but with:
-    -- card_id: 1001
-    -- ride_id: 5008
-    -- start_date: '2024-11-13'
-    -- start_time: '10:00:00'
-    -- duration: 20
-    -- on_stop_id: 101
-    -- off_stop_id: 102
-    -- line_id: 2
+    ('2023-10-01', '08:30:00', 'CD5678', 1003), 
+     ('2023-10-01', '08:30:00', 'CD5678', 1004), 
+      ('2023-10-01', '08:30:00', 'CD5678', 1005), 
     
-	--  Remember also to show illustrative usage examples of how they work.
-    --  Example: see section 7 (page 17)
-    --  Example: see section 7 (page 17)
+   
+     ('2023-10-02', '09:00:00', 'EF9012', 1002), 
+    
+    ('2023-10-02', '09:30:00', 'GH3456', 1003);
 
 
--- -- Queries ver 2 -- --
-
--- view for the displaying the card id of those who got on the first stop. 
-create view full_ride_passenger as select card_id from Rides, Stops_Line where Stops_Line.stop_order = 1 AND Rides.on_stop_id = Stops_Line.stop_id;
-select * from full_ride_passenger;
-
--- view for the name of the bus stop served by most lines 
-create view popular_stop as select stop_id, count(Stops_Line.stop_id) from Stops_Line group by Stops_Line.stop_id LIMIT 1;
-select * from popular_stop;
-
--- view for each line, the ID of the passenge who took the ride that lasted longer. ie. the max durations for the different lines
-create view longest_ride as select card_id, MAX(duration), line_id from Rides group by line_id;
-select * from longest_ride;
-
--- view for the Id of the passengers who never took a bus line more than once per day
-create view one_ride_passenger as select card_id FROM Rides GROUP BY card_id, line_id, start_date HAVING COUNT(ride_id) = 1;
-select * from one_ride_passenger;
-
--- view for name of busstop never used (ie. never start nor end stop for any ride)
-create view lonely_stop as select stop_id from Stops where stop_id not in (SELECT distinct on_stop_id FROM Rides UNION SELECT DISTINCT off_stop_id FROM Rides);
-select * from lonely_stop;
+INSERT INTO Stops_Line (line_id, stop_id, stop_order) VALUES
+    (1, 101, 1),
+    (1, 102, 2),
+    (1, 103, 3),
+    (1, 106, 4);
 
 
--- !!Views!! Ver.2  --
+INSERT INTO Stops_Passengers (stop_id, card_id) VALUES
+    (101, 1001),
+    (103, 1003);
 
--- bus lines going through the central point
-create view central_bermuda as select line_id from Stops_line where stop_id = "101";
-select * from central_bermuda;
--- rides that are under 30 minutes "short ride"
-create view short_ride as select ride_id from Rides where duration < 30;
-select * from short_ride;
--- rides that are equal + longer than 30 minutes "long rides"
-create view long_ride as select ride_id from Rides where duration >= 30;
-select * from long_ride;
--- all start and end stops of lines 
-create view edge_stops as select stop_id from Stops_line where stop_order = 1 or stop_order = (select max(stop_order) from Stops_line);
+
+INSERT INTO PhoneNumbers (number, card_id) VALUES
+    ('1234567890', 1001),
+    ('0987654321', 1001),
+    ('1122334455', 1002),
+    ('2233445566', 1003);
+ ALTER TABLE Line
+  ADD CONSTRAINT fk_final_destination FOREIGN KEY (final_destination) REFERENCES Stops(stop_id);
+ 
